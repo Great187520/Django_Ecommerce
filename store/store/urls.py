0cde98b84062ec840f235b0fae87fc25930014c7
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-06-03 15:56:22 -0700

    Delete store/store directory

--- store/store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout")
-]
